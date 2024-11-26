e33f164e9db484a1bb42d98a2fa63af1c1c3ca15
TatianaManika <tati.manika@gmail.com>
2024-11-26 16:04:25 +0200

    Create Salon.java

---
+++ Salon.java
@@ -0,0 +1,52 @@
+public class Salon {
+  private int salonId;
+  private String name;
+  private String address;
+  private String location;
+  private String phoneNumber;
+  private String email;
+  private double rating;
+ 
+  public int getSalonId() {
+    return salonId;
+  }
+  public void setSalonId(int salonId) {
+    this.salonId = salonId;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getAddress() {
+    return address;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  public String getLocation() {
+    return location;
+  }
+  public void setLocation(String location) {
+    this.location = location;
+  }
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public double getRating() {
+    return rating;
+  }
+  public void setRating(double rating) {
+    this.rating = rating;
+  }
+}
