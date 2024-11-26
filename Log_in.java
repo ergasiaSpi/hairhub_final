3a5b0bf5f87045eb16240f7c1d922498520852f3
Gianniskyr230 <kyr2005giannis@gmail.com>
2024-11-26 14:33:14 +0200

    code for sign Up/In

---
+++ Log_in.java
@@ -0,0 +1,23 @@
+package ergasia_spineli;
+
+import java.util.Scanner;
+
+public class Log_in {
+
+    private static Scanner input = new Scanner(System.in);
+
+    private static void Get_Input() {
+        String Username = " ";
+        String Password = " ";
+        do{
+            System.out.println("Username: ");
+            Username = input.nextLine();
+            System.out.println("Password: ");
+            Password = input.nextLine();
+        } while (SQL_CON.SQL_SELECT(Username, Password)==false);
+    }
+    
+    public static void main(String[] args) {
+        Get_Input();
+    }
+}
