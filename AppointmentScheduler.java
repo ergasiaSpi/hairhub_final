ab95106b977a54aa788a725bb31766c257aff4ca
Nefeli8220042 <t8220042@aueb.gr>
2025-01-09 17:48:45 +0200

    Update AppointmentScheduler.java

--- AppointmentScheduler.java
+++ AppointmentScheduler.java
@@ -6,36 +6,19 @@ import java.sql.Statement;
 
 public class AppointmentScheduler {
 
+    private Connection connection;
 
-    private static final String DB_URL = "jdbc:sqlite:salon.db";
-
-    
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL);
+    //Σύνδεση με ΒΔ
+    public AppointmentScheduler(String db) throws SQLException {
+        connection =  DriverManager.getConnection(db);
     }
 
     // Μέθοδος για κλείσιμο ραντεβού
-    public boolean bookAppointment(int userId, int salonId, int stylistId, int serviceId, date date, time timeStart) {
+    public boolean bookAppointment(int userId, int salonId, int stylistId, int serviceId, date date, time timeStart, time timeEnd) {
         
-        // Εύρεση διάρκειας 
-        int duration = getServiceDuration(serviceId);
-        if (duration == -1) {
-            System.out.println("Η υπηρεσία δεν βρέθηκε.");
-            return false;
-        }
-
-        // Υπολογισμός ώρας λήξης
-        String timeEnd = calculateEndTime(timeStart, duration);
-
-        // Έλεγχος διαθεσιμότητας κομμωτή
-        if (!isStylistAvailable(stylistId, date, timeStart, timeEnd)) {
-            System.out.println("Ο κομμωτής δεν είναι διαθέσιμος για αυτή την ώρα.");
-            return false;
-        }
-
         // Εισαγωγή του ραντεβού στον πίνακα Appointments
-        String query = "INSERT INTO Appointments (user_id, salon_id, stylist_id, service_id, date, time_start, status) " +
-                       "VALUES (" + userId + ", " + salonId + ", " + stylistId + ", " + serviceId + ", '" + date + "', '" + timeStart + "', 'pending')";
+        String query = "INSERT INTO Appointments (user_id, salon_id, stylist_id, service_id, date, time_start, time_end) " +
+                       "VALUES (" + userId + ", " + salonId + ", " + stylistId + ", " + serviceId + ", " + date + ", " + timeStart + ", " + timeEnd )";
         try (Connection conn = getConnection();
             Statement stmt = conn.createStatement()) {
           
@@ -47,41 +30,69 @@ public class AppointmentScheduler {
          }
     
     }
+   
+    private List<String> getStylistsBySalonId(int salon_Id) {
+        List<String> stylistNames = new ArrayList<>();
+        String query = "SELECT stylist_name FROM stylists WHERE salon_id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query)
+            
+        preparedStatement.setInt(1, salon_Id);
 
-    // Μέθοδος για εύρεση της διάρκεια μιας υπηρεσίας
-    private int getServiceDuration(int serviceId) {
-        String query = "SELECT duration FROM Services WHERE service_id = " + serviceId;
-        try (Connection conn = getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query)) {
+        ResultSet resultSet = preparedStatement.executeQuery();
 
-       if (rs.next()) {
-           return rs.getInt("duration");
-       }
-   } catch (SQLException e) {
-       e.printStackTrace();
-   }
-        return -1; 
+        while (resultSet.next()) {
+            stylistNames.add(resultSet.getString("stylist_name"));
+        }
+        
+        return stylistNames;
     }
 
-    // Υπολογισμός ώρας λήξης
-    private String calculateEndTime(time timeStart, int duration) {
-        String[] parts = timeStart.split(":");
+    private List<String> getServices() {
+        List<String> serviceTypes = new ArrayList<>();
+        String query = "SELECT service_type FROM services";
+        PreparedStatement preparedStatement = connection.prepareStatement(query)) 
+    
 
-        int hours = Integer.parseInt(parts[0]);
-        int minutes = Integer.parseInt(parts[1]) + duration;
+        ResultSet resultSet = preparedStatement.executeQuery();
 
-        hours += minutes / 60;
-        minutes %= 60;
-
-        return String.format("%02d:%02d", hours, minutes);
+        while (resultSet.next()) {
+            serviceTypes.add(resultSet.getString("service_type"));
+        }
+        
+        return serviceTypes;
     }
 
+
+    
     // Main
     public static void main(String[] args) {
         AppointmentScheduler scheduler = new AppointmentScheduler();
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Δάλεξε το salon που επιθυμείς:");
+        int salonId = scanner.nextInt();
+        
+        List<String> stylists = getStylistsBySalonId(salonId);
 
-        boolean success = scheduler.bookAppointment(1, 1, 2, 3, "2024-12-01", "10:00");
+        System.out.println("Stylists για το Salon ID " + salonId + ":");
+        
+        for (int i = 0; i < stylists.size(); i++) {
+            System.out.println((i + 1) + ". " + stylists.get(i));
+        }
+
+        System.out.println("Διάλεξε έναν αριθμό από τη λίστα:");
+        int choice = scanner.nextInt();
+
+         if (choice > 0 && choice <= stylists.size()) {
+                
+            } else {
+                System.out.println("Μη έγκυρη επιλογή.");
+            }
+        
+        scanner.close();
+        
+    
+        boolean success = scheduler.bookAppointment( userId, salonId, stylistId, serviceId, date, timeStart, timeEnd);
 
         if (success) {
             System.out.println("Το ραντεβού κλείστηκε επιτυχώς!");
