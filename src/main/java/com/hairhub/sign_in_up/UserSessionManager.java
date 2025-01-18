0312af69282f5e26b63210efdb0900f5fe4e6b18
trougoner <hraklistrougakos@gmail.com>
2025-01-18 23:47:05 +0200

    Update UserSessionManager.java

--- src/main/java/com/hairhub/sign_in_up/UserSessionManager.java
+++ src/main/java/com/hairhub/sign_in_up/UserSessionManager.java
@@ -8,26 +8,26 @@ public class UserSessionManager {
     private static Integer currentStylistId = null;
 
 
-    // Method to sign in a user
+ 
     public static void signInUser(int userId) {
         isUserSignedIn = true;
         signedInUserId = userId;
         System.out.println("User with ID " + userId + " is signed in.");
     }
 
-    // Method to sign out a user
+   
     public static void signOutUser() {
         isUserSignedIn = false;
         signedInUserId = -1;
         System.out.println("User signed out.");
     }
 
-    // Method to check if a user is signed in
+     
     public static boolean isUserSignedIn() {
         return isUserSignedIn;
     }
 
-    // Method to get the currently signed-in user's ID
+    
     public static int getSignedInUserId() {
         if (isUserSignedIn) {
             return signedInUserId;
@@ -47,7 +47,7 @@ public class UserSessionManager {
     public static void setCurrentStylistId(int stylistId) {
         currentStylistId = stylistId;
     }
-    // Μέθοδος για λήψη του salon_id
+     
     public static Integer getCurrentSalonId() {
         return currentSalonId;
     }
